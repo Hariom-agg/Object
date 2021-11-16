c05b2a6c4cf18de2a919e2f2af6e8ddd6bfed9e1
Hariom-agg <74823540+Hariom-agg@users.noreply.github.com>
2021-11-16 15:51:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 video = "";
 objectDetector = "";
 Status = ""; 
+objects=[];
 
 function preload()
 {
@@ -11,7 +12,7 @@ function preload()
 
 function setup()
 {
-    canvas = createCanvas(480,380);
+    canvas = createCanvas(400,300);
     canvas.center();
 }
 
@@ -27,11 +28,41 @@ function modelLoaded()
     Status = "true";
     video.loop();
     video.speed(1);
-    video.volume(1);
+    video.volume(0.5);
 }
 
 
 function draw()
 {
-  image = (video , 0 ,0, 480 , 380);
+  image(video , 0 ,0, 400 , 300);
+
+  if(Status == "true")
+  {
+      objectDetector.detect(video, gotResults);
+
+      for(i=0; i<objects.length; i++)
+      {
+          document.getElementById("status").innerHTML = "STATUS: Objects Detected";
+          document.getElementById("Number_of_objects").innerHTML = "Number of objects detected are " + objects.length;
+          percent = floor(objects[i].confidence * 100)+ "%" ;
+          fill("red")
+          text(objects[i].label + " " + percent , objects[i].x+15 , objects[i].y+15);
+         noFill();
+         stroke("red");
+         rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+      }
+  }
+}
+
+function gotResults(error,results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+
+    console.log(results);
+    objects = results;
+
+    
 }
